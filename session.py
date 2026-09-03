de582b52d19aee0019ddbf9fb1a090448620eb76
teleshaapp-dotcom <telesha.app@gmail.com>
2026-09-03 23:39:38 +0300

    Delete session.py

--- session.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-
-api_id = 35712521
-api_hash = "b0713b67f41a77cb3271d49f84705d08" # (ئەگەر هاشەکەت گۆڕی، ئەمەش بگۆڕە)
-
-with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
-    session_string = app.export_session_string()
-    print("\n🔑 کۆدی String Session ەکەت ئەمەیە:\n")
-    print(session_string)
